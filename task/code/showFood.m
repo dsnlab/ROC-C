0e2c1074ba01e034040e8041abe2f14c1323059d
Dani Cosme <dani.cosme@gmail.com>
2018-04-19 20:36:12 -0700

    fixed duration info

--- task/code/showFood.m
+++ task/code/showFood.m
@@ -16,11 +16,8 @@ else
     ISI = Jitter(trial);
 end
 
-% Display fixation 
-%DrawFormattedText(PTBParams.win,'+','center','center',PTBParams.white);
-%TrialStart = Screen(PTBParams.win,'Flip');
+% Define trial start
 trialStart=GetSecs;
-fixDuration = (trialStart-StartTime)-fixOnset;
 
 % Display food
 foodCoords = findPicLoc(size(foodJpg{foodTrial}),[.5,.45],PTBParams,'ScreenPct',.55);
@@ -31,7 +28,7 @@ foodOn = Screen(PTBParams.win,'Flip', trialStart+ISI);
 
 % Display fixation cross
 DrawFormattedText(PTBParams.win,'+','center','center',PTBParams.white);
-fixRatingsOn = Screen(PTBParams.win,'Flip', foodOn+foodWait);
+fixOn = Screen(PTBParams.win,'Flip', foodOn+foodWait);
 
 % Display rating prompt after time specified in WaitTime
 Screen(PTBParams.win,'TextSize',round(.15*PTBParams.ctr(2)));
@@ -40,15 +37,16 @@ Screen(PTBParams.win,'TextSize',round(.15*PTBParams.ctr(2)));
 DrawFormattedText(PTBParams.win,'no desire',.65*posRate1_x,posPress_y,PTBParams.blue);
 DrawFormattedText(PTBParams.win,'strong desire',.97*posRate2_x,posPress_y,PTBParams.blue);
 DrawFormattedText(PTBParams.win,'\n\n1    -------    2    -------    3    -------   4','center',posPress_y,PTBParams.white);
-ratingOn = Screen(PTBParams.win,'Flip',fixRatingsOn+fixRatings);
+ratingOn = Screen(PTBParams.win,'Flip',fixOn+fixRatings);
 Screen('Close',FoodScreen);
 
 % Log trial info
 foodPic = char(jpgs(foodTrial));
 foodNum = foodTrial;
 foodOnset = foodOn-StartTime;
+fixOnset = fixOn-StartTime;
 ratingOnset = ratingOn-StartTime;
-foodDuration = ratingOnset-foodOnset;
+foodDuration = fixOnset-foodOnset;
 cond = char(trialOrder(trial));
 likingRating = ratings(find(strcmp(foodPic, images)));
 if sum(strcmp(char(foodPic),eval(sprintf('%s_craved',runNum)))) == 1
