f7abdc4ab72717c178323d28ced4232f2194e1dd
Dani Cosme <dani.cosme@gmail.com>
2018-07-05 10:10:36 -0700

    fixed debugging printing

--- task/code/showFood.m
+++ task/code/showFood.m
@@ -15,8 +15,10 @@ Screen('FrameRect', PTBParams.win, color, foodCoords, 10);
 foodOn = Screen(PTBParams.win,'Flip');
 WaitSecs(foodWait);
 foodOnset = foodOn-StartTime;
-if blockTrial == 1; fprintf('fixCue dur = %.1f\n', foodOnset - fixOnset); end
-if blockTrial > 1; fprintf('ITI dur = %.1f, ITI = %.1f\n', foodOnset - itiOnset, ITI); end
+if PTBParams.debugging == 1
+    if blockTrial == 1; fprintf('fixCue dur = %.1f\n', foodOnset - fixOnset); end
+    if blockTrial > 1; fprintf('ITI dur = %.1f, ITI = %.1f\n', foodOnset - itiOnset, ITI); end
+end
 
 % Display fixation cross
 DrawFormattedText(PTBParams.win,'+','center','center',PTBParams.white);
@@ -24,7 +26,7 @@ fixOn = Screen(PTBParams.win,'Flip');
 WaitSecs(fixRatings);
 fixRatingOnset = fixOn-StartTime;
 foodDuration = fixRatingOnset-foodOnset;
-fprintf('food dur = %.1f\n', foodDuration);
+if PTBParams.debugging == 1; fprintf('food dur = %.1f\n', foodDuration); end
 
 % Specify trial info for logging
 foodPic = char(jpgs(foodTrial));
